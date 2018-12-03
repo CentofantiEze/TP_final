9c31c78520f6ad9f5709dc745bcd9a32e16e4426
kevinmicha <44092360+kevinmicha@users.noreply.github.com>
2018-12-03 04:41:31 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -1,106 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
-#include "arg_load.c"
-#include "structures.c"
-#include "trackpoint.c"
 
-int main(int argc, const char ** argv) {
+#define MAX_PROCESS_T 3
 
-	arg_s * metadata = NULL;
+
+typedef enum {PROCESS_ZERO, PROCESS_ONE, PROCESS_TWO, PROCESS_ALL} process_t;
+
+
+int main(int argc, const char argv **) {
+
+	arg_s * metadata;
 	status_t st;
-	List * list = NULL;
+	tkpt_s * tkpt;
+	List * list;
 	process_t ran;
-	status_t (*get_trackpoint[])(FILE *, void **) = {get_trackpoint_nmea, get_trackpoint_ubx};
-	int data_protocol;
-	void * data_structs[DATA_S_TYPES];
 
 	srand(time(NULL));
 
 	metadata = arg_create(&st);
 
 	if(st != ST_OK) {
-		free_metadata(metadata);
-		close_files(metadata);
-		return st;
+		/* REPORTAR ERRORES */
+		return 0; /* EXIT_FAILURE/SUCCESS LIBERAR MEMORIA Y CERRAR ARCHIVOS */
 	}
 
 	st = arg_load(argc, argv, metadata);
 
 	if(st != ST_OK) {
-		free_metadata(metadata);
-		close_files(metadata);
-		return st;
+		/* REPORTAR ERRORES */
+		return 0; /* EXIT_FAILURE/SUCCESS LIBERAR MEMORIA Y CERRAR ARCHIVOS */
 	}
 
-	/***************************************************************************/
+	status_t protocol_detect(metadata_t *); // ####
 
-	if(metadata->protocol == P_AUTO)
-		if((st = protocol_detect(metadata)) != ST_OK) {
-			free_metadata(metadata);
-			close_files(metadata);
-			return 0;
-		}
+	if(list_create(list) != ST_OK)
+		return ST_NO_MEM; /* LIBERAR MEMORIA Y CERRAR ARCHIVOS */
 
-	if(list_create(list) != ST_OK) {
-		free_metadata(metadata);
-		close_files(metadata);
-		return ST_NO_MEM;
-	}
+	while(! (tkpt = get_trackpoint(metadata))) {
 
-	if((st = data_structs_create(metadata, data_structs)) != ST_OK) {
-		free_metadata(metadata);
-		close_files(metadata);
-		list_delete(list);
-		return st;
-	}
-
-
-	data_protocol = metadata->protocol - 1;
-
-	while(get_trackpoint[data_protocol](metadata->infile, data_structs)) { // preguntar
-
-		if((st = list_append_tkpt(list, data_structs[DATA_TKPT_S], metadata->maxlen)) != ST_OK) {
-			free_metadata(metadata);
-			close_files(metadata);
-			list_delete(list);
-			free_data_structs(data_structs);
-			return st;
-		}
+		if(list_append(list, tkpt, metadata->maxlen) != ST_OK) // ####
+			return ERROR; /* LIBERAR MEMORIA Y CERRAR ARCHIVOS */
 
 		ran = rand() % MAX_PROCESS_T;
 
-		if((st = gpx_process(list, ran)) != ST_OK) { // ####
-			free_metadata(metadata);
-			close_files(metadata);
-			list_delete(list);
-			free_data_structs(data_structs);
-			return st;
-		}
+		if(gpx_process(list, ran) != ST_OK) // ####
+			return ERROR; /* LIBERAR MEMORIA Y CERRAR ARCHIVOS */
 
 	}
 
-	gpx_process(list, PROCESS_ALL);
+	gpx_process(list, PROCESS_ALL)
 
-	free_metadata(metadata);
-	close_files(metadata);
-	list_delete(list);
-	free_data_structs(data_structs);
-	return EXIT_SUCCESS; 
+	close_all(metadata)
+
+	free_all(list, tkpt, )
+
+	return EXIT_SUCCESS;
 
 }
 
 
-/*
+tkpt_t * get_trackpoint(arg_s * metadata) {
 
-TO DO:
+	if(metadata->protocol = P_NMEA)
+		return get_trackpoint_nmea(metadata); // ###
 
-status_t gpx_process(List *, proces_t);
+	return get_trackpoint_ubx(metadata); // ###
 
-*/
-
-
-
-
+}
