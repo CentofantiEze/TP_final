30d4fa75e29d26ac371db4ba6a47675d61bbde68
kevinmicha <44092360+kevinmicha@users.noreply.github.com>
2018-12-05 09:23:33 -0300

    Update logs.c

--- logs.c
+++ logs.c
@@ -28,6 +28,6 @@ void log_print(FILE * f, log_t current_log) {
 
 	fprintf(f, "\n%s", log_dictionary[current_log]);
 
-	if(current_log == UBX_LONG || current_log == UBX_TIME || current_log == UBX_DATE || current_log == INCORRECT_STATEMENT || current_log == INTERNAL_ERROR || current_long == CHECKSUM)
-		fprintf(f, "\n%s", log_dictionary[MSG_IGNORED_STATEMENT]);	
+	if(current_log == UBX_LONG || current_log == UBX_TIME || current_log == UBX_DATE || current_log == INCORRECT_STATEMENT || current_log == INTERNAL_ERROR || current_log == CHECKSUM)
+		fprintf(f, "\n%s", log_dictionary[IGNORED_STATEMENT]);	
 }
